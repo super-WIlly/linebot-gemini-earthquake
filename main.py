9121916988c002ac0e5d2c4923c9ad1b098cdd0e
super-WIlly <b10922023@gemail.yuntech.edu.tw>
2024-07-06 22:44:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,183 +1,51 @@
-import logging
-import os
-import re
-import sys
-if os.getenv('API_ENV') != 'production':
-    from dotenv import load_dotenv
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
 
-    load_dotenv()
+def generate():
+  vertexai.init(project="anti-fraud-chatbot", location="us-central1")
+  model = GenerativeModel(
+    "gemini-1.5-pro-001",
+    system_instruction=[textsi_1]
+  )
+  responses = model.generate_content(
+      [text1],
+      generation_config=generation_config,
+      safety_settings=safety_settings,
+      stream=True,
+  )
 
+  for response in responses:
+    print(response.text, end="")
 
-from fastapi import FastAPI, HTTPException, Request
-from datetime import datetime
-from linebot.v3.webhook import WebhookParser
-from linebot.v3.messaging import (
-    AsyncApiClient,
-    AsyncMessagingApi,
-    Configuration,
-    ReplyMessageRequest,
-    TextMessage)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
+text1 = """那應該會愛惜房屋吧我就是想找一個愛惜的租客所以租金方面都算得很優惠啦~
+15:20
+會喔愛惜房屋是必要的!!房租真的有感動到!
+15:23
+了解是打算長租還是短租呢
+15:26
+已讀
+15:26
+目前想法是半年
+好的看屋要禮拜三,你在第五組看屋,請問你方便嗎
+15:31
+已讀 請問您什麼時候幾點方便
+15:34
+具體看屋時間我會通知你,因為目前第三位的租客打算預付訂金優先看屋,如果前面的租客看屋滿意簽約了房屋後面的租客就沒有機會看屋了"""
 
-import uvicorn
-import requests
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+}
 
-logging.basicConfig(level=os.getenv('LOG', 'WARNING'))
-logger = logging.getLogger(__file__)
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
 
-app = FastAPI()
+generate()
 
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-configuration = Configuration(
-    access_token=channel_access_token
-)
-
-async_api_client = AsyncApiClient(configuration)
-line_bot_api = AsyncMessagingApi(async_api_client)
-parser = WebhookParser(channel_secret)
-
-
-import google.generativeai as genai
-from firebase import firebase
-from utils import check_image_quake, check_location_in_message, get_current_weather, get_weather_data, simplify_data
-
-
-firebase_url = os.getenv('FIREBASE_URL')
-gemini_key = os.getenv('GEMINI_API_KEY')
-
-
-# Initialize the Gemini Pro API
-genai.configure(api_key=gemini_key)
-
-
-@app.get("/health")
-async def health():
-    return 'ok'
-
-
-@app.post("/webhooks/line")
-async def handle_callback(request: Request):
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = await request.body()
-    body = body.decode()
-
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="Invalid signature")
-
-    for event in events:
-        logging.info(event)
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-        text = event.message.text
-        user_id = event.source.user_id
-
-        msg_type = event.message.type
-        fdb = firebase.FirebaseApplication(firebase_url, None)
-        if event.source.type == 'group':
-            user_chat_path = f'chat/{event.source.group_id}'
-        else:
-            user_chat_path = f'chat/{user_id}'
-            chat_state_path = f'state/{user_id}'
-        chatgpt = fdb.get(user_chat_path, None)
-
-        if msg_type == 'text':
-
-            if chatgpt is None:
-                messages = []
-            else:
-                messages = chatgpt
-
-            bot_condition = {
-                "清空": 'A',
-                "摘要": 'B',
-                "地震": 'C',
-                "氣候": 'D',
-                "其他": 'E'
-            }
-
-            model = genai.GenerativeModel('gemini-1.5-pro')
-            response = model.generate_content(
-                f'請判斷 {text} 裡面的文字屬於 {bot_condition} 裡面的哪一項？符合條件請回傳對應的英文文字就好，不要有其他的文字與字元。')
-            print('='*10)
-            text_condition = re.sub(r'[^A-Za-z]', '', response.text)
-            print(text_condition)
-            print('='*10)
-            if text_condition == 'A':
-                fdb.delete(user_chat_path, None)
-                reply_msg = '已清空對話紀錄'
-            elif text_condition == 'B':
-                model = genai.GenerativeModel('gemini-pro')
-                response = model.generate_content(
-                    f'Summary the following message in Traditional Chinese by less 5 list points. \n{messages}')
-                reply_msg = response.text
-            elif text_condition == 'C':
-                print('='*10)
-                print("地震相關訊息")
-                print('='*10)
-                model = genai.GenerativeModel('gemini-pro-vision')
-                OPEN_API_KEY = os.getenv('OPEN_API_KEY')
-                earth_res = requests.get(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-003?Authorization={OPEN_API_KEY}&downloadType=WEB&format=JSON')
-                url = earth_res.json()["cwaopendata"]["Dataset"]["Resource"]["ProductURL"]
-                reply_msg = check_image_quake(url)+f'\n\n{url}'
-            elif text_condition == 'D':
-                location_text = '台北市'
-                location = check_location_in_message(location_text)
-                print('Location is: ' + location)
-                weather_data = get_weather_data(location)
-                simplified_data = simplify_data(weather_data)
-                current_weather = get_current_weather(simplified_data)
-
-                print('The Data is: ' + str(current_weather))
-
-                now = datetime.now()
-                formatted_time = now.strftime("%Y/%m/%d %H:%M:%S")
-
-                if current_weather is not None:
-                    total_info = f'位置: {location}\n氣候: {current_weather["Wx"]}\n降雨機率: {current_weather["PoP"]}\n體感: {current_weather["CI"]}\n現在時間: {formatted_time}'
-
-                response = model.generate_content(
-                    f'你現在身處在台灣，相關資訊 {total_info}，我朋友說了「{text}」，請問是否有誇張、假裝的嫌疑？ 回答是或否。')
-                reply_msg = response.text
-            else:
-                # model = genai.GenerativeModel('gemini-pro')
-                messages.append({'role': 'user', 'parts': [text]})
-                response = model.generate_content(messages)
-                messages.append({'role': 'model', 'parts': [text]})
-                # 更新firebase中的對話紀錄
-                fdb.put_async(user_chat_path, None, messages)
-                reply_msg = response.text
-
-            await line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=reply_msg)]
-                ))
-
-    return 'OK'
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', default=8080))
-    debug = True if os.environ.get(
-        'API_ENV', default='develop') == 'develop' else False
-    logging.info('Application will start...')
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=debug)
